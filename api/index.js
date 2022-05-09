033f2300fb25e5e4c5c5d8dc2f8a52646d3e30df
Ysmael Marques <ysmaelmarkss@gmail.com>
2022-05-09 18:01:29 -0300

    teste3"

--- api/index.js
+++ api/index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const routes = require('./routes')
 
 const app = express();
+const port = 3000;
 
 routes(app);    
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+app.listen(port, ()=> console.log(`servidor rodando na porta ${port}`))
 
 module.exports = app
