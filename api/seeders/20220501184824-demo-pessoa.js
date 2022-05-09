dc0d3f25ed028a415e883a0d0e775f33ec2a2a6d
Ysmael Marques <ysmaelmarkss@gmail.com>
2022-05-09 20:54:20 -0300

    teste9

--- api/seeders/20220501184824-demo-pessoa.js
+++ api/seeders/20220501184824-demo-pessoa.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('pessoas', [
+    await queryInterface.bulkInsert('Pessoas', [
       {
 				nome: 'Ana Souza',
 				ativo: true,
@@ -56,6 +56,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
 
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('Pessoas', null, {});
   }
 };
